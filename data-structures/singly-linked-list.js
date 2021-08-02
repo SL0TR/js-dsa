fbcdeea835ac8c7fc0b85719ff46a4cdb3cb8fee
SL0TR <mohaiminislam94@gmail.com>
2021-08-02 23:53:32 +0600

    chore: finish single linked list reverse method

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -79,7 +79,7 @@ class SinglyLinkedList {
 
   get(index) {
     if (index < 0 || index > this.length) {
-      return null;
+      return false;
     }
 
     let counter = 0;
@@ -97,9 +97,95 @@ class SinglyLinkedList {
     let indexNode = this.get(index);
     if (indexNode) {
       indexNode.val = val;
-      return this;
+      return true;
     }
-    return null;
+    return false;
+  }
+
+  insert(val, index) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+
+    if (index === this.length) {
+      return !!this.push(val);
+    }
+
+    if (!this.head || index === 0) {
+      return !!this.unshift(val);
+    }
+    const newNode = new Node(val);
+    const prevNode = this.get(index - 1);
+    const currentNode = prevNode.next;
+    newNode.next = currentNode;
+    prevNode.next = newNode;
+
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length || !this.head) {
+      return false;
+    }
+
+    if (index === this.length) {
+      return this.pop();
+    }
+
+    if (index === 0) {
+      return this.shift();
+    }
+
+    const prevNode = this.get(index - 1);
+    const indexNode = prevNode.next;
+    prevNode.next = indexNode.next;
+    this.length--;
+    return indexNode;
+  }
+
+  print() {
+    let string = "";
+    let current = this.head;
+
+    while (current) {
+      string += `${current.val}${current.next ? " --> " : ""}`;
+      current = current.next;
+    }
+
+    return string;
+  }
+
+  reverse() {
+    let current = this.head;
+    this.head = this.tail;
+    this.tail = current;
+
+    let prev = null;
+    let next = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    return this;
+  }
+
+  reverse2(head) {
+    let prev = null;
+    let next = null;
+    let current = head;
+    while (current !== null) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    return prev;
   }
 }
 
@@ -109,8 +195,13 @@ singlyLinkedList.push("wazzup");
 singlyLinkedList.push("hey!");
 singlyLinkedList.push("who dat");
 singlyLinkedList.push("in yo face");
-
-console.log(singlyLinkedList.unshift("puntastic"));
-console.log(singlyLinkedList.unshift("you are"));
-console.log(singlyLinkedList.get(1));
-console.log(singlyLinkedList.set("very puntastic", 1));
+// console.log(singlyLinkedList.unshift("puntastic"));
+// console.log(singlyLinkedList.unshift("you are"));
+// console.log(singlyLinkedList.set("very very puntastic", 1));
+// console.log(singlyLinkedList.get(1));
+// console.log(singlyLinkedList.insert("very puntastic", 1));
+// console.log(singlyLinkedList.remove(0));
+// console.log(singlyLinkedList.print());
+// console.log(singlyLinkedList.reverse2(singlyLinkedList.head));
+console.log(singlyLinkedList.reverse());
+// console.log(singlyLinkedList.print());
