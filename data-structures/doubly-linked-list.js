b526451606d89f80d8c9c46e7d7d78b784fa8e4d
SL0TR <mohaiminislam94@gmail.com>
2021-08-04 01:24:11 +0600

    chore add get, set, insert and remove method on dll ds

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -82,13 +82,103 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    const middle = Math.floor(this.length / 2);
+    let counter = 0;
+    let current = this.head;
+    let key = "next";
+
+    if (index > middle) {
+      current = this.tail;
+      key = "prev";
+      index = this.length - index - 1;
+    }
+
+    while (counter !== index) {
+      current = current[key];
+      counter++;
+    }
+
+    return current;
+  }
+
+  set(index, val) {
+    const indexNode = this.get(index);
+
+    if (indexNode) {
+      indexNode.val = val;
+      return true;
+    }
+
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+
+    if (index === this.length) {
+      return !!this.push(val);
+    }
+
+    if (!this.head || index === 0) {
+      return !!this.unshift(val);
+    }
+
+    const newNode = new Node(val);
+    const indexNode = this.get(index);
+    const beforeIndexNode = indexNode.prev;
+
+    newNode.prev = indexNode.prev;
+    newNode.next = indexNode;
+    indexNode.prev = newNode;
+    beforeIndexNode.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+
+    if (index === this.length) {
+      return !!this.pop(val);
+    }
+
+    if (index === 0) {
+      return !!this.shift(val);
+    }
+
+    const indexNode = this.get(index);
+    const beforeIndexNode = indexNode.prev;
+    const afterIndexNode = indexNode.next;
+
+    beforeIndexNode.next = indexNode.next;
+    beforeIndexNode.prev = indexNode.prev;
+    afterIndexNode.prev = beforeIndexNode;
+
+    this.length--;
+    return this;
+  }
 }
 
 const doublyLinkedList = new DoublyLinkedList();
 
-console.log(doublyLinkedList.push(1));
-console.log(doublyLinkedList.push(2));
-console.log(doublyLinkedList.push(3));
+new Array(15).fill("").forEach((_, i) => {
+  doublyLinkedList.push(i + 1);
+});
+
 // console.log(doublyLinkedList.pop());
 // console.log(doublyLinkedList.shift());
-console.log(doublyLinkedList.unshift(0));
+// console.log(doublyLinkedList.unshift(0));
+// console.log(doublyLinkedList.get(7));
+// console.log(doublyLinkedList.set(7, 10));
+// console.log(doublyLinkedList.insert(7, 8));
+console.log(doublyLinkedList.remove(3));
