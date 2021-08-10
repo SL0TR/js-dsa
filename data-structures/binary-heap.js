eeec81dacbd18b4a1d7a71823e45af3aeb442434
SL0TR <mohaiminislam94@gmail.com>
2021-08-10 23:35:23 +0600

    implement extraMax method for t he binary heap

--- data-structures/binary-heap.js
+++ data-structures/binary-heap.js
@@ -1,6 +1,6 @@
 class BinaryHeap {
   constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [55, 39, 41, 18, 27, 12, 33];
   }
 
   insert(val) {
@@ -25,12 +25,67 @@ class BinaryHeap {
       this.values[itemIndex] = parentItem;
       itemIndex = parentIndex;
     }
+  }
 
-    console.log({ bh: this.values });
+  extractMax() {
+    const maxItem = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+    this.sinkDown();
+
+    console.log(this.values);
+
+    return maxItem;
+  }
+
+  sinkDown() {
+    let parentIdx = 0;
+    const length = this.values.length;
+    const parentItem = this.values[parentIdx];
+
+    while (true) {
+      let leftChildIdx = 2 * parentIdx + 1;
+      let rightChildIdx = 2 * parentIdx + 2;
+
+      let leftChild, rightChild;
+
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+
+        if (leftChild > parentItem) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+
+        if (
+          (!swap && rightChild > parentItem) ||
+          (swap && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) {
+        break;
+      }
+
+      this.values[parentIdx] = this.values[swap];
+      this.values[swap] = parentItem;
+      parentIdx = swap;
+    }
+
+    return this.values;
   }
 }
 
 const bh = new BinaryHeap();
 
-bh.insert(55);
-bh.insert(155);
+// bh.insert(55);
+// bh.insert(155);
+
+console.log(bh.extractMax());
